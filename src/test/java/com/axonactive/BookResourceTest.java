776d0fb04c1cbb5017b8ec3ddd847ab19374ddc0
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-11-17 15:34:18 +0700

    Quarkus testing with JUnit5 & RESTAssured

---
+++ src/test/java/com/axonactive/BookResourceTest.java
@@ -0,0 +1,42 @@
+package com.axonactive;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.HttpHeaders;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class BookResourceTest {
+
+    @Test
+    public void shouldGetAllBooks() {
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .when().get("/books")
+                .then()
+                .statusCode(200)
+                .body("size()", is(2));
+    }
+
+    @Test
+    public void shouldGetNumberOfTheBooks() {
+        given().header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN)
+                .when().get("/books/count")
+                .then().statusCode(200).body(is("2"));
+    }
+
+    @Test
+    public void shouldGetBookById() {
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).pathParam("id", 1)
+                .when().get("/books/{id}")
+                .then().statusCode(200)
+                .body("id", is(1))
+                .body("name", is("Book 2"))
+                .body("author", is("Author 2"))
+                .body("yearOfPublication", is(2020))
+                .body("genre", is("Genre2"));
+    }
+}
--- src/test/java/com/axonactive/GreetingResourceIT.java
+++ src/test/java/com/axonactive/GreetingResourceIT.java
@@ -3,6 +3,6 @@ package com.axonactive;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
+public class GreetingResourceIT extends BookResourceTest {
     // Execute the same tests but in packaged mode.
 }
